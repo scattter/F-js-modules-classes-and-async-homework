9903176b361bbaac8115ee93597aaf1ba6513105
scattter <413365742@qq.com>
2020-09-14 17:50:13 +0800

    [zk] finsh practice

--- src/practice1/utils.js
+++ src/practice1/utils.js
@@ -1,1 +1,4 @@
-const getURL
+export default function getURL(address, port, path) {
+    return address + ":" + port + path;
+}
+
--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,19 @@
-export const getPoetry
+export function getPoetry() {
+    return fetch("https://v1.jinrishici.com/all.json")
+        .then((response) => {
+            if (response.ok) {
+                return Promise.resolve(response.json())
+            }
+            return Promise.reject(new Error(fail))
+        })
+        .then((data) => {
+            var result = []
+            result.push(data.origin)
+            result.push(data.author)
+            result.push(data.content)
+            return result
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+}
