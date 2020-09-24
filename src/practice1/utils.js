c19178bb57a474d8f4e8d637ea9fd1d78fb78a48
xijing.gui <xijing.gui@thoughtworks.com>
2020-09-24 16:23:40 +0800

    add feedback

--- src/practice1/utils.js
+++ src/practice1/utils.js
@@ -1,4 +1,5 @@
 export default function getURL(address, port, path) {
+    // TODO feedback: 推荐使用ES6的知识拼接字符串
     return address + ":" + port + path;
 }
 
--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,12 +1,14 @@
 export function getPoetry() {
     return fetch("https://v1.jinrishici.com/all.json")
         .then((response) => {
+          // TODO feedback：直接return response.json()就可以了
             if (response.ok) {
                 return Promise.resolve(response.json())
             }
             return Promise.reject(new Error("fail"))
         })
         .then((data) => {
+          // TODO feedback: 可以用ES6新特性解构result
             var result = []
             result.push(data.origin)
             result.push(data.author)
